8454944e6ee5b66c9d5c27ea3001d58023a5b9c6
WinstonWolf007 <ubguertin@gmail.com>
2022-01-28 18:37:33 -0500

    create fist pygame screen

---
+++ Official/game.py
@@ -0,0 +1,13 @@
+class Game:
+    def __init__(self):
+        self.screenSize = (800, 800)
+        self.screenTitle = "Socket Game"
+        self.screenRun = True
+
+        self.color = {
+            'white': (255, 255, 255),
+            'black': (0, 0, 0),
+            'red': (255, 0, 0),
+            'green': (0, 255, 0),
+            'blue': (0, 0, 255)
+        }
