55b1b8e15ca320d48ba1502028fed0c6493b74dc
WinstonWolf007 <ubguertin@gmail.com>
2022-02-14 14:22:32 -0500

    finish display player instance for each player
    to start change position for vew game

--- Official/player1/client.py
+++ Official/player1/client.py
@@ -1,5 +1,6 @@
 # import...
 import pygame
+import json
 from game import Game, OnlinePlayer
 
 # init instance
@@ -7,29 +8,46 @@ pygame.init()
 game = Game()
 
 # call server (for generate new player) and (get information all player)
-allPlayerId = game.CallEvent('get:players:id')
+"""allPlayerId = game.CallEvent('get:players:id').split(':')
 allPlayerPosition = game.CallEvent('get:players:position')
 alonePlayer = False
 
-if '204' not in [allPlayerId, allPlayerPosition]: # check there are one player or more online
+print(allPlayerPosition)
+if allPlayerId != '204' and allPlayerPosition != '204': # check there are one player or more online
+    print('online !')
     nbrPlayer = len(allPlayerId)
     onlinePlayer = OnlinePlayer(nbrPlayer)
+    print(onlinePlayer.players)
     for i in onlinePlayer.players:
-        eachPlayer = onlinePlayer.players.get(f'player{i}')
-        selfId = eachPlayer.avatar['id']
-        eachPlayer.rect = allPlayerPosition[selfId]
+        eachPlayer = onlinePlayer.players.get(f'{i}')
+        #selfId = eachPlayer.avatar['id']
+        #eachPlayer.rect = allPlayerPosition[selfId]
+        print(eachPlayer.avatar)
 else:
-    alonePlayer = True
+    alonePlayer = True"""
 
-game.CallEvent(game.allEvent[0], game.player.avatar)
-
-# config pygame screen
+# create screen
 screen = pygame.display.set_mode(game.screenSize)
 pygame.display.set_caption(game.screenTitle)
 coloringScreen = lambda: screen.fill(game.color['white'])
 pygame.display.flip()
 loop = True
 
+# Call server: new player -> server give rule (player position, player id)
+game_rule = json.loads(game.CallEvent(game.allEvent[0], game.player.avatar))
+number_player_online = len(game_rule)-1
+game_rule_online = {}
+
+for i in game_rule:
+    if i != game.player.avatar['id']:
+        game_rule_online[i] = game_rule[i]
+
+# create the other player if there are
+if game_rule_online != 0:
+    online_player = OnlinePlayer(number_player_online)
+
+
+# Game loop
 while game.screenRun:
     coloringScreen()
     for event in pygame.event.get():
@@ -57,9 +75,9 @@ while game.screenRun:
         pass
 
     # display all player if not alone or one player if alone
-    if not alonePlayer:
-        for idPlayer in allPlayerId:
-            screen.blit(onlinePlayer.players[idPlayer].image, onlinePlayer.players[idPlayer].image)
+    if number_player_online != 0:
+        for i in range(number_player_online):
+            screen.blit(online_player.players[f'player{i}'].image, online_player.players[f'player{i}'].rect)
 
     screen.blit(game.player.image, game.player.rect)
     pygame.display.update()
--- Official/server.py
+++ Official/server.py
@@ -5,7 +5,7 @@ import json
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 port = 10_000
-all_players = {}
+all_players = {"123": {'position': [0, 34]}}
 
 try:
     s.bind((host, port))
@@ -27,33 +27,12 @@ def threaded_client(connection):
                 all_players.pop(profile['id'])
                 connection.sendall(b'200')
             elif profile['Event'] == 'players:new':
-                all_players[profile['id']] = {}
-                connection.sendall(b'200')
+                all_players[profile['id']] = {'position': profile['position']}
+                connection.sendall(str.encode(json.dumps(all_players)))
             elif profile['Event'] == 'players:move':
                 all_players[profile['id']]['position'] = profile['position']
                 connection.sendall(b'200')
-
-            # return value to the client
-            elif profile['Event'] == 'get:players':
-                connection.sendall(str.encode(json.dumps(all_players)))
-            elif profile['Event'] == 'get:players:id':
-                allId = ''
-                for playerId in all_players:
-                    allId += playerId+':'
-                allId = allId[:-1]
-                if allId:
-                    connection.sendall(str.encode(allId))
-                else:
-                    connection.sendall(b'204')
-
-            elif profile['Event'] == 'get:players:position':
-                all_position = {}
-                try:
-                    for playerId2 in all_players:
-                        all_position[playerId2] = all_players[playerId2]['position']
-                    connection.sendall(json.dumps(all_position))
-                except:
-                    connection.sendall(b'204')
+        print(all_players)
 
 while True:
     Client, address = s.accept()
